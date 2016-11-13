ac85328ee9bbb6ab7f13bdb9d728e5dfd9f95a3b
r_takaishi <ryo.takaishi.0@gmail.com>
2016-11-13 14:31:01 +0900

    change to input script path from stdin

--- mrblib/chimay.rb
+++ mrblib/chimay.rb
@@ -1,12 +1,15 @@
 def __main__(argv)
   if argv[1] == "version"
     puts "v#{Chimay::VERSION}"
-  elsif argv[1] =~ Regexp.compile("https?://[^/]+[-a-zA-Z0-9./]+")
-    Http.run(argv[1])
-  elsif argv[1] =~ Regexp.compile("s3://[^/]+[-a-zA-Z0-9./]+")
-    S3.run(argv[1])
   else
-    file = File.open(argv[1])
-    eval(file.read, nil, __FILE__, __LINE__)
+    input = STDIN.read
+
+    if input =~ Regexp.compile("^https?://[^/]+[-a-zA-Z0-9./]+")
+      Http.run(argv[1])
+    elsif input =~ Regexp.compile("^s3://[^/]+[-a-zA-Z0-9./]+")
+      S3.run(argv[1])
+    else
+      eval(input, nil, __FILE__, __LINE__)
+    end
   end
 end
