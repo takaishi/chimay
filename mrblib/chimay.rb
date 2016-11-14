1dafd2be7441d18c9fe1411efd1f32b262d1f15a
r_takaishi <ryo.takaishi.0@gmail.com>
2016-11-14 22:28:24 +0900

    add CLI class

--- mrblib/chimay.rb
+++ mrblib/chimay.rb
@@ -1,37 +1,5 @@
 def __main__(argv)
-  if argv[1] == "version"
-    puts "v#{Chimay::VERSION}"
-  else
-    parsed = parse_stdin(STDIN.read)
-
-    if parsed[:type] == :http
-      Http.run(parsed[:payload])
-    elsif parsed[:type] == :s3
-      S3.run(parsed[:payload])
-    elsif parsed[:type] == :file
-      File.run(parsed[:payload])
-    else
-    end
-  end
+  cli = CLI.new
+  cli.run(argv)
 end
 
-def parse_stdin(input)
-  if input =~ Regexp.compile("^https?://[^/]+[-a-zA-Z0-9./]+")
-    {:type => :http, :payload => input}
-  elsif input =~ Regexp.compile("^s3://[^/]+[-a-zA-Z0-9./]+")
-    {:type => :s3, :payload => input}
-  elsif input =~ Regexp.compile("^file://[^/]+[-a-zA-Z0-9./]+")
-    {:type => :file, :payload => input}
-  else
-    command = JSON::parse(input)[0]
-    payload =  Base64::decode(command['Payload'])
-    if payload =~ Regexp.compile("^https?://[^/]+[-a-zA-Z0-9./]+")
-      {:type => :http, :payload => payload}
-    elsif payload =~ Regexp.compile("^s3://[^/]+[-a-zA-Z0-9./]+")
-      {:type => :s3, :payload => payload}
-    elsif input =~ Regexp.compile("^file://[^/]+[-a-zA-Z0-9./]+")
-      {:type => :file, :payload => input}
-    else
-    end
-  end
-end
